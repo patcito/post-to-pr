f6921b9ce7e3c257a4fe101b1e3f2048d0c7db0a
Patrick Aljord <patcito@gmail.com>
2021-05-08 14:27:28 +0400

    feat: faundadb

--- src/pages/api/callback.ts
+++ src/pages/api/callback.ts
@@ -2,12 +2,30 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import fetch from 'node-fetch';
 import { load } from 'ts-dotenv';
 import github, { getOctokit } from '@actions/github';
+import faunadb from 'faunadb';
 
 const env = load({
   CLIENT_ID: String,
   CLIENT_SECRET: String,
+  FAUNA_SECRET: String,
   NODE_ENV: ['production' as const, 'development' as const],
 });
+
+const client = new faunadb.Client({
+  secret: env.FAUNA_SECRET,
+  domain: 'db.fauna.com',
+  scheme: 'https',
+});
+const q = faunadb.query;
+
+client
+  .query(q.Collections())
+  .then(function (res) {
+    console.log('Result:', res);
+  })
+  .catch(function (err) {
+    console.log('Error:', err);
+  });
 
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 export default async (req: NextApiRequest, res: NextApiResponse) => {
@@ -28,6 +46,14 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     });
 
     const text = await request.text();
+    console.log(text);
+    client
+      .query(
+        q.Create(q.Collection('userRepoToken'), {
+          data: { text },
+        }),
+      )
+      .then((ret) => console.log(ret));
     res.json({
       body,
       text: text,
