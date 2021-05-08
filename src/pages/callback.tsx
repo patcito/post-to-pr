fb3a9c5ebb45600476618ff9ba4f3a2714310e9c
Patrick Aljord <patcito@gmail.com>
2021-05-08 19:50:29 +0400

    fix: fix: save home page

--- src/pages/callback.tsx
+++ src/pages/callback.tsx
@@ -3,9 +3,6 @@ import dynamic from 'next/dynamic';
 import Head from 'next/head';
 import { useEffect, useState } from 'react';
 import styles from '../styles/Home.module.css';
-type AccessToken = {
-  access_token: string;
-};
 
 export default function Home() {
   const StartingBlock = dynamic(() => import('../components/starting'));
